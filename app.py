9d5a0630b4910a305fa69034c445a7d7bf9cafaf
hahalissatan <hahalissatan@gmail.com>
2026-01-13 16:12:58 +0800

    now that the drone can take off and fly to the location that i set. the next step is to update the status

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-SIM_KEY = 'fe32be9d-238f-4478-bae0-cb008b2f320c' 
+SIM_KEY = '8d510f6d-e1e4-440e-a957-fb58d0683fa2' 
 
 # 1. THE HOME PAGE
 @app.route('/')
@@ -29,19 +29,34 @@ def order_page():
 def dispatch():
     # Get the data sent from the JavaScript
     data = request.get_json()
-    matric = data.get('matric')
     location = data.get('location')
-    item = data.get('item')
 
-    # 1. Send the Telegram message to HEP
-    send_hep_alert(matric, location, item)
-
-    # 2. Run the DroneBlocks Code
+    # Run the DroneBlocks Code
     with DroneBlocksSimulatorContextManager(simulator_key=SIM_KEY) as drone:
         drone.takeoff()
-        drone.fly_forward(40, 'in')
-        drone.land()
         
+        # Logic based on the 'H' marks on the pad
+        if location == "Block A1":
+            drone.fly_forward(100, 'cm')
+            drone.fly_left(100, 'cm')
+        elif location == "Block B2":
+            drone.fly_forward(100, 'cm')
+            drone.fly_right(100, 'cm')
+        elif location == "Dewan Kuliah":
+            drone.fly_forward(100, 'cm')
+        elif location == "Kafeteria":
+            drone.fly_right(100, 'cm')
+        elif location == "Langkasuka":
+            drone.fly_backward(100, 'cm')
+        elif location == "Perpustakaan":
+            drone.fly_left(100, 'cm')
+        else: 
+            drone.fly_backward(100,'cm')
+            drone.fly_right(100, 'cm')
+        
+        # Land the drone
+        drone.land()
+
     return "Dispatched!"
 
 if __name__ == "__main__":
